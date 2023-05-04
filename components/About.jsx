dd3bdf8f00af49eb70fed677033d88e0baf75aa1
Wasif Khan <ukhanwasif@gmail.com>
2023-05-04 12:58:03 +0500

    About me section replaced

--- components/About.jsx
+++ components/About.jsx
@@ -13,25 +13,29 @@ const About = () => {
           </p>
           <h2 className="py-4">Who I Am</h2>
           <p className="py-2 text-gray-600">
-            I specialize in building mobile responsive front-end UI applications
-            that connect with API’s and other backend technologies. I’m
-            passionate about learning new technologies and understand there is
-            more than one way to accomplish a task. Though I am most proficient
-            in building front-end applications using HTML, CSS, Javascript, and
-            React, I am a quick learner and can pick up new tech stacks as
-            needed. I believe that being a great developer is not using one
-            specific language, but choosing the best tool for the job.
+            I am a full-stack web developer with a passion for frontend
+            development. With 2 years of professional experience in frontend
+            development, I have developed a deep understanding of HTML,Css,
+            ReactJS, NextJS, NodeJS, ExpressJS, MongoDB, SQL, Git, and Amazon
+            Web Services (AWS) . My approach to work is driven by a desire to
+            solve complex problems, Innovations and bringing new ideas to life .
+            I take a collaborative approach and thrive in environments where I
+            can work with others to achieve common goals. Throughout my career,
+            I have had the opportunity to work on a wide range of projects and
+            initiatives.
+            <Link href="/#projects"> You can find some of them below.</Link>
           </p>
           <p className="py-2 text-gray-600">
-            I started my self-taught web developement jounrey in 2020, I had
-            started web development as a fun. Subsequently, my interest for it
-            got broader and I started loving to work with forms data & would
-            always be curious to connect the server and the frontend. Due to
-            that I decided not to focus only on the frontend but the backend as
-            well. So for the backend I learned and worked with AWS and after
-            working with AWS it made me more interested in the backend, cloud
-            computing and I started digging it deeply.
+            In my free time, I enjoy finding new open source projects on Github
+            that i can contribute to and learning and exploring tech tools that
+            are trending such as ChatGPT. I find that these activities help me
+            to recharge and approach my work with renewed energy and creativity.
+            I am excited to continue to grow and develop my skills and
+            experiences, and I am always open to new opportunities and
+            challenges. Thank you for taking the time to learn more about me,
+            and I look forward to connecting with you further.
           </p>
+
           <Link href="/#projects">
             <p className="py-2 text-gray-600 underline cursor-pointer">
               Check out some of my latest projects.
